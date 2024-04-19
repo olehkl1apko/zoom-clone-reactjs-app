dfdce4992633083c629e9e4affbd5116ff17eab3
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-19 17:46:32 +0300

    ADD: basic server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
+const twilio = require("twilio");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
