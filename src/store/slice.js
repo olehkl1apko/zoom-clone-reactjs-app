f0d28294b61c18dc0593c2beeca0a11c6b536b41
olehkl1apko <kliapko.oleh.dev@gmail.com>
2024-04-20 09:11:30 +0300

    ADD: redux store

---
+++ src/store/slice.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// Initial state
+const initialState = {
+  identity: "",
+  isRoomHost: false,
+  connectOnlyWithAudio: false,
+  roomId: null,
+  showOverlay: true,
+  participants: [],
+  messages: [],
+  activeConversation: null,
+  directChatHistory: [],
+  socketId: null,
+};
+
+// Create slice
+const videoChatSlice = createSlice({
+  name: "videoChat",
+  initialState,
+  reducers: {
+    setIsRoomHost: (state, action) => {
+      state.isRoomHost = action.payload;
+    },
+    setConnectOnlyWithAudio: (state, action) => {
+      state.connectOnlyWithAudio = action.payload;
+    },
+    setRoomId: (state, action) => {
+      state.roomId = action.payload;
+    },
+    setIdentity: (state, action) => {
+      state.identity = action.payload;
+    },
+    setShowOverlay: (state, action) => {
+      state.showOverlay = action.payload;
+    },
+    setParticipants: (state, action) => {
+      state.participants = action.payload;
+    },
+    setMessages: (state, action) => {
+      state.messages = action.payload;
+    },
+    setActiveConversation: (state, action) => {
+      state.activeConversation = action.payload;
+    },
+    setDirectChatHistory: (state, action) => {
+      state.directChatHistory = action.payload;
+    },
+    setSocketId: (state, action) => {
+      state.socketId = action.payload;
+    },
+  },
+});
+
+export const {
+  setIsRoomHost,
+  setConnectOnlyWithAudio,
+  setRoomId,
+  setIdentity,
+  setShowOverlay,
+  setParticipants,
+  setMessages,
+  setActiveConversation,
+  setDirectChatHistory,
+  setSocketId,
+} = videoChatSlice.actions;
+
+export default videoChatSlice.reducer;
